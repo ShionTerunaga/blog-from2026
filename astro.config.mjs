d04ac4d9aaaf1bae1427490561de07f6dcbfc172
ShionTerunaga <sion2769@gmail.com>
2026-02-08 00:37:43 +0900

    feat: set cloude flare

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,16 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import { defineConfig } from "astro/config";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
   integrations: [mdx(), sitemap()],
+
   vite: {
     plugins: [vanillaExtractPlugin()],
   },
+
+  adapter: cloudflare(),
 });
